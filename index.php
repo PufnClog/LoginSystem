a359c788dd327accf38ead02e6cd7ba42b7daab5
Elliot Wright <wright.elliot@gmail.com>
2012-09-29 16:41:34 +0100

    This is kind of how you'd do it... in this case.
    But it's bad. Don't do it like this normally!

--- index.php
+++ index.php
@@ -1,61 +1,82 @@
 <?php
-    /*
-     *~~~~~~Practice 1: Winging it~~~~~~
-     *Author:Zoe Wood
-     *Summary: Login System using php and mySQL LETS DO THIS!!!
-     */
 
-    // Connect to database NOTE TO SELF: really need to look into this using classes
+	/**
+	 * ~~~~~~Practice 1: Winging it~~~~~~
+	 * 
+	 * Author:Zoe Wood
+	 * Summary: Login System using php and mySQL LETS DO THIS!!!
+	 */
 
-    session_start(); // Sets up login session for the user
+	// Connect to database NOTE TO SELF: really need to look into this using classes
+
+	session_start(); // Starts PHP sessions.
+
+	require_once('./handlers/database.handler.php');
+
+	$objDatabase = new DatabaseHandler('localhost', 'root', 'Diablo2');
+
+	// If the username and password have been sent to the server:
+	if( isset( $_POST['username']) && isset( $_POST['password']) )
+	{
+		$strUserName = mysql_real_escape_string( $_POST['username'] );
+		$strUserPass = mysql_real_escape_string( $_POST['password'] );
+
+		$strQuery = 'SELECT strUserName, '.
+		                  ' strUserPass '.
+		              'FROM testlogin.tbl_user '.
+		             "WHERE strUserName='$strUserName' ".
+		               "AND strUserPass='$strUserPass'";
+
+		$objUser = mysql_query( $strQuery );
+		$arrUser = mysql_fetch_assoc( $objUser );
+
+		if( $arrUser )
+		{
+			// If the login was sucessful:
+			$_SESSION['username'] = $arrUser['strUserName'];
+			$_SESSION['password'] = $arrUser['strUserPass'];
+
 ?>
-
-<?php
-    require_once('./handlers/database.handler.php');
-
-    $objDatabase = new DatabaseHandler('localhost', 'root', 'local');
-
-    //USERNAME AND PASSWORD SENT FROM FORM
-    if( isset( $_POST['username']) && isset( $_POST['password']) )
-    {
-        $strUserName = mysql_real_escape_string( $_POST['username'] );
-        $strUserPass = mysql_real_escape_string( $_POST['password'] );
-
-        $strQuery = 'SELECT strUserName '.
-                         ', strUserPass '.
-                      'FROM testlogin.tbl_user '.
-                     "WHERE strUserName='$strUserName' ".
-                       "AND strUserPass='$strUserPass'";
-
-        $objUser = mysql_query( $strQuery );
-        $arrUser = mysql_fetch_assoc( $objUser );
-
-    }
-
-    if(//hmmmm it seems ive encountered a problem here better ask the sensei xD im pretty sure $arrUser is the key variable i need here but how do i present it without it logging in EVERYTHING...)
-    {
-        $_SESSION['username'] = $strUserName;           //If Login is Successful
-        $_SESSION['password'] = $strUserPass;
-        echo "Login Successful";
-     }else{                              //If it isnt...
-        echo "Incorrect Login, try again";
-    }
-//Notes so far (man its getting late -.-)... something is DEFINATELY missing wth is it >.<
-//Need to see what sessions are all about still probably getting ahead of myself again ^^'
-//Also button? that is all... might need something like a $bolLoggedIn=false like i did with arrays hmmmm
-?>
-
 
 <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Login Practice</title>
-        </head>
-        <body>
-            <form method="POST">
-                <input name="username" type="text" placeholder="Username">
-                <input name="password" type="password" placeholder="Password">
-                <input type="submit">
-            </form
-        </body>
-    </html>
+<html>
+	<head>
+		<title>Login Practice</title>
+	</head>
+	<body>
+		<h1>Welcome back!</h1>
+		<p>How've you been <?php echo $_SESSION['username']; ?>. We've missed you!</p>
+	</body>
+</html>
+
+<?php
+
+		}
+		else
+		{
+			// If the login was unsuccessful:
+			echo "Incorrect Login, try again";
+		}
+	} else {
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login Practice</title>
+	</head>
+	<body>
+		<form method="POST">
+			<input name="username" type="text" placeholder="Username">
+			<input name="password" type="password" placeholder="Password">
+			<input type="submit">
+		</form>
+	</body>
+</html>
+
+<?php
+	
+	}
+
+?>
